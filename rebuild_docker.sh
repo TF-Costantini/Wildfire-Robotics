0c33eabfecd71a4669fb241efeb0798177083f72
margheritagaiazzi <margherita.gaiazzi@mail.polimi.it>
2026-04-25 18:02:57 +0200

    update rebuild_docker.sh

--- rebuild_docker.sh
+++ rebuild_docker.sh
@@ -12,11 +12,13 @@ docker pull $IMAGE_NAME
 docker container rm -f $CONTAINER_NAME
 
 #Runs ros image
-docker run -it \
+docker run -itd \
   --name $CONTAINER_NAME \
   -v "$PROJ_FILES/src/ros2_ws/src:/home/project/src/ros2_ws" \
   -v "$PROJ_FILES/start.sh:/start.sh" \
   -v "$PROJ_FILES/test_images:/home/project" \
   $IMAGE_NAME
 
-echo -e "\nSTARTED SUCCESSFULLY"
+echo -e "\nSTARTED SUCCESSFULLY\n"
+
+docker exec -it $CONTAINER_NAME /bin/bash
