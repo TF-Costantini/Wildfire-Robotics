e22c0c9b81714b3a3af1b69ea6f5614a13065aa7
margheritagaiazzi <margherita.gaiazzi@mail.polimi.it>
2026-04-25 16:42:40 +0200

    updated script to use relative paths

--- run_docker.sh
+++ run_docker.sh
@@ -3,7 +3,7 @@ set -e
 
 IMAGE_NAME=ros:humble
 CONTAINER_NAME=ros2_dev
-PROJ_FILES=/Users/margheritagaiazzi/Desktop/Wildfire-Robotics
+PROJ_FILES=./_LINUX/src
 
 #Pulls ROS image
 docker pull $IMAGE_NAME
