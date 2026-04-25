9e3512f61a8d946fe9d180b2a7f5b86b3df42a39
margheritagaiazzi <margherita.gaiazzi@mail.polimi.it>
2026-04-25 16:29:12 +0200

    commit file sh docker

---
+++ run_docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+IMAGE_NAME=ros:humble
+CONTAINER_NAME=ros2_dev
+PROJ_FILES=/Users/margheritagaiazzi/Desktop/Wildfire-Robotics
+
+#Pulls ROS image
+docker pull $IMAGE_NAME
+
+#Removes container
+docker container rm -f $CONTAINER_NAME
+
+#Runs ros image
+docker run -it --name $CONTAINER_NAME -v "$PROJ_FILES:/home/project" $IMAGE_NAME
+
+echo -e "\nSTARTED SUCCESSFULLY"
