6b61500edad6a62efb10497dbd2aff78e378b18c
TF-Costantini <tommaso.fcostantini@gmail.com>
2026-04-28 23:33:04 +0200

    .

--- _RTOS/adb_deploy_RTOS.sh
+++ _RTOS/adb_deploy_RTOS.sh
@@ -9,4 +9,4 @@ DEVICE_SERIAL="3038275042"
 
 adb -s "$DEVICE_SERIAL" shell "mkdir -p $REMOTE_DIR"
 adb -s "$DEVICE_SERIAL" push "$BIN_DIR" "$REMOTE_DIR"
-adb -s "$DEVICE_SERIAL" shell "arduino-cli upload --fqbn arduino:zephyr:unoq --input-file $REMOTE_DIR/main.ino.elf"
+adb -s "$DEVICE_SERIAL" shell "arduino-cli upload -v --fqbn arduino:zephyr:unoq --input-file $REMOTE_DIR/main.ino.elf"
