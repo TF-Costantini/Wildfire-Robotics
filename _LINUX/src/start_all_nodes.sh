f18b8721dd624930ee6a14922c97645550f4816f
TF-Costantini <tommaso.fcostantini@gmail.com>
2026-04-30 23:36:40 +0200

    other updates, minor

--- _LINUX/src/start_all_nodes.sh
+++ _LINUX/src/start_all_nodes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-nohup ros2 launch wildfire_bringup bringup.launch.py use_sim:=true > ~/all_nodes.log 2>&1 &
+nohup ros2 launch wildfire_bringup bringup.launch.py use_sim:=false > ~/all_nodes.log 2>&1 &
