9ccd0dc481c3b7190424940a86594ff06dc55d18
TF-Costantini <tommaso.fcostantini@gmail.com>
2026-05-01 20:32:37 +0200

    RPC for commands to mcu

--- _LINUX/src/ros2_ws/src/wildfire_control/wildfire_control/comm_bridge_node.py
+++ _LINUX/src/ros2_ws/src/wildfire_control/wildfire_control/comm_bridge_node.py
@@ -21,14 +21,46 @@ class UnoQBridgeNode(Node):
     def __init__(self):
         super().__init__('comm_bridge_node')
 
-        self.ultrasonic_left_pub  = self.create_publisher(Range, 'ultrasonic/left', 10)
-        self.ultrasonic_right_pub = self.create_publisher(Range, 'ultrasonic/right', 10)
+        self._pantilt_sub = self.create_subscription(
+            PanTiltCmd,
+            '/cmd_pantilt',
+            self._on_pantilt,
+            10
+        )
+
+        self._laser_sub = self.create_subscription(
+            Bool,
+            '/cmd_laser',
+            self._on_laser,
+            10
+        )
+
+        self._drive_sub = self.create_subscription(
+            DriveCmd,
+            '/cmd_drive',
+            self._on_drive,
+            10
+        )
+
+        self.ultrasonic_left_pub  = self.create_publisher(Range, '/ultrasonic/left', 10)
+        self.ultrasonic_right_pub = self.create_publisher(Range, '/ultrasonic/right', 10)
         self.button_pub           = self.create_publisher(ButtonEvent, '/button_event', 10)
 
         Bridge.provide("on_ultrasonic", self.on_ultrasonic)
         Bridge.provide("on_button", self.on_button)
 
         self.get_logger().info("UNO Q Bridge node started")
+
+    def _on_pantilt(self, msg: PanTiltCmd):
+        Bridge.call("set_pantilt", msg.pan_deg, msg.tilt_deg)
+        return
+
+    def _on_laser(self, msg: Bool):
+        Bridge.call("set_laser", msg.data)
+        return
+    def _on_drive(self, msg):
+        Bridge.call("set_drive", msg.left, msg.right)
+        return
 
     def on_ultrasonic(self, left_m: float, right_m: float):
         stamp = self.get_clock().now().to_msg()
