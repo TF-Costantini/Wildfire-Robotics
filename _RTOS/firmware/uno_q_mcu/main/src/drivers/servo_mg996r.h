c678bf6b50cee24edb0266b9d106ec61ec16c23d
TF-Costantini <tommaso.fcostantini@gmail.com>
2026-05-26 10:18:29 +0200

    minor updates

--- _RTOS/firmware/uno_q_mcu/main/src/drivers/servo_mg996r.h
+++ _RTOS/firmware/uno_q_mcu/main/src/drivers/servo_mg996r.h
@@ -14,13 +14,13 @@ extern "C" {
 void servo_init(void);
 
 /** Set pan angle in degrees (clamped to configured limits). */
-void servo_pan_set(float deg);
+void servo_pan_set(int in);
 
 /** Set tilt angle in degrees (clamped to configured limits). */
-void servo_tilt_set(float deg);
+void servo_tilt_set(int in);
 
 /** Set pan and tilt at once. */
-void servo_set_position(float pan_deg, float tilt_deg);
+void servo_set_position(int pan_deg, int tilt_deg);
 
 /** Move both servos to (0, 0). */
 void servo_home(void);
@@ -29,11 +29,11 @@ void servo_home(void);
 void servo_safe_position(void);
 
 /** Read back last commanded angles. NULL pointers are skipped. */
-void servo_get_position(float *pan, float *tilt);
+void servo_get_position(int *pan, int *tilt);
 
 /** Override the default soft limits. */
-void servo_set_limits(float pan_min, float pan_max,
-                      float tilt_min, float tilt_max);
+void servo_set_limits(int pan_min, int pan_max,
+                      int tilt_min, int tilt_max);
 
 #ifdef __cplusplus
 }
