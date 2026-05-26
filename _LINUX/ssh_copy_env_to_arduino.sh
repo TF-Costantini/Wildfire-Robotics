b71330db7a7d1cc7824f0ba6513e71463e0fbada
TF-Costantini <tommaso.fcostantini@gmail.com>
2026-05-26 18:34:19 +0200

    updated ssh_copy_env_to_arduino.sh

--- _LINUX/ssh_copy_env_to_arduino.sh
+++ _LINUX/ssh_copy_env_to_arduino.sh
@@ -6,19 +6,14 @@ ARDUINO_BASE="/home/arduino"
 PEM_PATH=/Volumes/POLI/.ssh/auq_phoenix.pem
 REMOTE=arduino@phoenix.local
 
-echo -e "\nCOPYING ALL REQUIRED DATA...\n"
+echo -e "\nSYNCING...\n"
 
-ssh -i "$PEM_PATH" "$REMOTE" "rm -rf $ARDUINO_BASE$BASE_PATH"
-ssh -i "$PEM_PATH" "$REMOTE" "mkdir -p $ARDUINO_BASE$BASE_PATH/src/"
-ssh -i "$PEM_PATH" "$REMOTE" "mkdir -p $ARDUINO_BASE$BASE_PATH/test_images/"
+rsync -avz --delete -e "ssh -i $PEM_PATH" \
+  "..$BASE_PATH/" "$REMOTE:$ARDUINO_BASE$BASE_PATH/"
 
-scp -i "$PEM_PATH" -r "..$BASE_PATH/src/." "$REMOTE:$ARDUINO_BASE$BASE_PATH/src/"
-scp -i "$PEM_PATH" -r "..$BASE_PATH/test_images/." "$REMOTE:$ARDUINO_BASE$BASE_PATH/test_images"
-scp -i "$PEM_PATH" -r "..$BASE_PATH/setup.sh" "$REMOTE:$ARDUINO_BASE$BASE_PATH/setup.sh"
-scp -i "$PEM_PATH" -r "..$BASE_PATH/start.sh" "$REMOTE:$ARDUINO_BASE$BASE_PATH/start.sh"
-scp -i "$PEM_PATH" -r "../rebuild_docker.sh" "$REMOTE:$ARDUINO_BASE/rebuild_docker.sh"
-scp -i "$PEM_PATH" -r "../start_docker.sh" "$REMOTE:$ARDUINO_BASE/start_docker.sh"
+rsync -avz -e "ssh -i $PEM_PATH" \
+  "../rebuild_docker.sh" "../start_docker.sh" \
+  "$REMOTE:$ARDUINO_BASE/"
 
 echo -e "\nRESTARTING CONTAINER...\n"
 ssh -i "$PEM_PATH" "$REMOTE" "docker restart ros2_dev"
-
